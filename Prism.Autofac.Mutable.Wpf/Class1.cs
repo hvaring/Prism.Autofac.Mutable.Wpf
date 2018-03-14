b02e2c6d1dde4f4cc92ae3bf75bbb460282d02af
hvaring <hvaring@gmail.com>
2018-03-15 00:29:03 +0100

    Add project files.

---
+++ Prism.Autofac.Mutable.Wpf/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism.Autofac.Mutable.Wpf
+{
+    public class Class1
+    {
+    }
+}
